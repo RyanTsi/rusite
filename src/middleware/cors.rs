abbd9aaf6efd782458849eaaf8452a8ca90d0d0a
solaryan <466010106@qq.com>
2025-06-15 00:00:33 +0800

    fix: CORS bug.

--- src/middleware/cors.rs
+++ src/middleware/cors.rs
@@ -5,7 +5,7 @@ use actix_web::http::header;
 pub fn CORS() -> Cors {
     Cors::default()
         .allow_any_origin()
-        .allowed_methods(vec!["GET", "POST"])
+        .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
         .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
         .allowed_header(header::CONTENT_TYPE)
         .supports_credentials()
